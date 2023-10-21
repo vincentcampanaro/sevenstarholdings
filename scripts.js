887270eda3d272129dc0599cfe8830e856d7421b
Vincent Campanaro <124087048+vincentcampanaro@users.noreply.github.com>
2023-10-21 01:38:25 -0400

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,1 @@
-document.getElementById('management').addEventListener('mouseover', function() {
-    this.style.backgroundColor = '#2980b9';
-});
 
-document.getElementById('management').addEventListener('mouseout', function() {
-    this.style.backgroundColor = '#3498db';
-});
-
-document.getElementById('vacations').addEventListener('mouseover', function() {
-    this.style.backgroundColor = '#27ae60';
-});
-
-document.getElementById('vacations').addEventListener('mouseout', function() {
-    this.style.backgroundColor = '#2ecc71';
-});
